4ac3712762b28442809875fd6b06dfda84614901
Denis Kalach <denskyline333@gmail.com>
2023-09-08 19:24:04 +0300

    f5.4full

---
+++ src/components/profile/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './profile.css'
+import ava from '../../img/elon.jpg'
+import ava_min from '../../img/elon_mini.jpg'
+
+function Profile(){
+	return(
+		<div className='profile'>
+			<div className='me'>
+				<img src={ava} alt="profile mini photo" /> 
+				<p>Elon Mask</p>
+			</div>
+			<div className='posts'>
+				<p>My posts</p>
+				<input placeholder='enter the post' />
+				<button>Add post</button>
+				<div className='post'>
+					<img src={ava_min} alt="profile mini photo" /> 
+					<span className="mini_name">Elon Mask</span>
+					<p>Some text</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+export default Profile
